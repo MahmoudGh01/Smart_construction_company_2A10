41bf8ea75dbb93556098d843ee16a41a273a37c1
Asma Glenza <asma.glenza@esprit.tn>
2022-03-07 00:32:10 +0100

    test

---
+++ build-Atelier_Connexion-Desktop_Qt_5_9_9_MinGW_32bit-Debug/ui_mainwindow.h
@@ -0,0 +1,280 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.9.9
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QGroupBox>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTabWidget>
+#include <QtWidgets/QTableView>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralWidget;
+    QTabWidget *tabWidget;
+    QWidget *tab;
+    QGroupBox *groupBox;
+    QLabel *label;
+    QLabel *label_2;
+    QLabel *label_3;
+    QPushButton *pushButton_2;
+    QLineEdit *lineEdit;
+    QLineEdit *lineEdit_2;
+    QLineEdit *lineEdit_3;
+    QLabel *label_13;
+    QLabel *label_12;
+    QWidget *tab_2;
+    QTableView *tableView;
+    QLabel *label_11;
+    QLabel *label_16;
+    QWidget *tab_3;
+    QLabel *label_4;
+    QLineEdit *lineEdit_4;
+    QPushButton *pushButton_3;
+    QLabel *label_7;
+    QLabel *label_14;
+    QWidget *tab_4;
+    QLineEdit *id_modif;
+    QPushButton *modif_button;
+    QLineEdit *nom_modif;
+    QLineEdit *prix_modif;
+    QLabel *label_8;
+    QLabel *label_9;
+    QLabel *label_10;
+    QLabel *label_6;
+    QLabel *label_15;
+    QLabel *label_5;
+    QMenuBar *menuBar;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QStringLiteral("MainWindow"));
+        MainWindow->resize(679, 495);
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        tabWidget = new QTabWidget(centralWidget);
+        tabWidget->setObjectName(QStringLiteral("tabWidget"));
+        tabWidget->setGeometry(QRect(0, 0, 671, 431));
+        tab = new QWidget();
+        tab->setObjectName(QStringLiteral("tab"));
+        groupBox = new QGroupBox(tab);
+        groupBox->setObjectName(QStringLiteral("groupBox"));
+        groupBox->setGeometry(QRect(10, 10, 801, 351));
+        label = new QLabel(groupBox);
+        label->setObjectName(QStringLiteral("label"));
+        label->setGeometry(QRect(210, 40, 171, 31));
+        label_2 = new QLabel(groupBox);
+        label_2->setObjectName(QStringLiteral("label_2"));
+        label_2->setGeometry(QRect(240, 110, 171, 31));
+        label_3 = new QLabel(groupBox);
+        label_3->setObjectName(QStringLiteral("label_3"));
+        label_3->setGeometry(QRect(240, 190, 171, 31));
+        pushButton_2 = new QPushButton(groupBox);
+        pushButton_2->setObjectName(QStringLiteral("pushButton_2"));
+        pushButton_2->setGeometry(QRect(450, 280, 141, 41));
+        lineEdit = new QLineEdit(groupBox);
+        lineEdit->setObjectName(QStringLiteral("lineEdit"));
+        lineEdit->setGeometry(QRect(350, 40, 141, 31));
+        lineEdit_2 = new QLineEdit(groupBox);
+        lineEdit_2->setObjectName(QStringLiteral("lineEdit_2"));
+        lineEdit_2->setGeometry(QRect(350, 110, 141, 31));
+        lineEdit_3 = new QLineEdit(groupBox);
+        lineEdit_3->setObjectName(QStringLiteral("lineEdit_3"));
+        lineEdit_3->setGeometry(QRect(350, 190, 141, 31));
+        label_13 = new QLabel(groupBox);
+        label_13->setObjectName(QStringLiteral("label_13"));
+        label_13->setGeometry(QRect(-20, -10, 661, 391));
+        label_13->setPixmap(QPixmap(QString::fromUtf8("../../../Desktop/bg de construction \342\200\223 Recherche\302\240Google_files/54919363-gestionnaire-homme-builder-et-travailleur-sur-la-constructions-de-b\303\242timents-de-fond-construction-et-.jpg")));
+        label_13->setScaledContents(true);
+        label_13->raise();
+        label->raise();
+        label_2->raise();
+        label_3->raise();
+        pushButton_2->raise();
+        lineEdit->raise();
+        lineEdit_2->raise();
+        lineEdit_3->raise();
+        label_12 = new QLabel(tab);
+        label_12->setObjectName(QStringLiteral("label_12"));
+        label_12->setGeometry(QRect(0, -50, 901, 631));
+        label_12->setPixmap(QPixmap(QString::fromUtf8("../../../../bandes-seules-occultant-beige-clair-3515.jpg")));
+        tabWidget->addTab(tab, QString());
+        label_12->raise();
+        groupBox->raise();
+        tab_2 = new QWidget();
+        tab_2->setObjectName(QStringLiteral("tab_2"));
+        tableView = new QTableView(tab_2);
+        tableView->setObjectName(QStringLiteral("tableView"));
+        tableView->setGeometry(QRect(10, 11, 731, 321));
+        label_11 = new QLabel(tab_2);
+        label_11->setObjectName(QStringLiteral("label_11"));
+        label_11->setGeometry(QRect(0, -10, 901, 521));
+        label_11->setPixmap(QPixmap(QString::fromUtf8("../../../../bandes-seules-occultant-beige-clair-3515.jpg")));
+        label_16 = new QLabel(tab_2);
+        label_16->setObjectName(QStringLiteral("label_16"));
+        label_16->setGeometry(QRect(10, 10, 631, 321));
+        label_16->setPixmap(QPixmap(QString::fromUtf8("../../../Desktop/77777777777777.jpg")));
+        label_16->setScaledContents(true);
+        tabWidget->addTab(tab_2, QString());
+        tableView->raise();
+        label_16->raise();
+        label_11->raise();
+        tab_3 = new QWidget();
+        tab_3->setObjectName(QStringLiteral("tab_3"));
+        label_4 = new QLabel(tab_3);
+        label_4->setObjectName(QStringLiteral("label_4"));
+        label_4->setGeometry(QRect(290, 140, 71, 31));
+        lineEdit_4 = new QLineEdit(tab_3);
+        lineEdit_4->setObjectName(QStringLiteral("lineEdit_4"));
+        lineEdit_4->setGeometry(QRect(350, 140, 141, 41));
+        pushButton_3 = new QPushButton(tab_3);
+        pushButton_3->setObjectName(QStringLiteral("pushButton_3"));
+        pushButton_3->setGeometry(QRect(490, 230, 111, 41));
+        label_7 = new QLabel(tab_3);
+        label_7->setObjectName(QStringLiteral("label_7"));
+        label_7->setGeometry(QRect(0, 0, 901, 521));
+        label_7->setPixmap(QPixmap(QString::fromUtf8("../../../../bandes-seules-occultant-beige-clair-3515.jpg")));
+        label_14 = new QLabel(tab_3);
+        label_14->setObjectName(QStringLiteral("label_14"));
+        label_14->setGeometry(QRect(0, 0, 631, 371));
+        label_14->setPixmap(QPixmap(QString::fromUtf8("../../../Desktop/bg de construction \342\200\223 Recherche\302\240Google_files/54919363-gestionnaire-homme-builder-et-travailleur-sur-la-constructions-de-b\303\242timents-de-fond-construction-et-.jpg")));
+        label_14->setScaledContents(true);
+        tabWidget->addTab(tab_3, QString());
+        label_14->raise();
+        label_7->raise();
+        label_4->raise();
+        lineEdit_4->raise();
+        pushButton_3->raise();
+        tab_4 = new QWidget();
+        tab_4->setObjectName(QStringLiteral("tab_4"));
+        id_modif = new QLineEdit(tab_4);
+        id_modif->setObjectName(QStringLiteral("id_modif"));
+        id_modif->setGeometry(QRect(390, 150, 151, 31));
+        modif_button = new QPushButton(tab_4);
+        modif_button->setObjectName(QStringLiteral("modif_button"));
+        modif_button->setGeometry(QRect(490, 280, 111, 41));
+        nom_modif = new QLineEdit(tab_4);
+        nom_modif->setObjectName(QStringLiteral("nom_modif"));
+        nom_modif->setGeometry(QRect(390, 70, 151, 31));
+        prix_modif = new QLineEdit(tab_4);
+        prix_modif->setObjectName(QStringLiteral("prix_modif"));
+        prix_modif->setGeometry(QRect(390, 220, 151, 31));
+        label_8 = new QLabel(tab_4);
+        label_8->setObjectName(QStringLiteral("label_8"));
+        label_8->setGeometry(QRect(280, 220, 171, 31));
+        label_9 = new QLabel(tab_4);
+        label_9->setObjectName(QStringLiteral("label_9"));
+        label_9->setGeometry(QRect(240, 70, 171, 31));
+        label_10 = new QLabel(tab_4);
+        label_10->setObjectName(QStringLiteral("label_10"));
+        label_10->setGeometry(QRect(290, 150, 171, 31));
+        label_6 = new QLabel(tab_4);
+        label_6->setObjectName(QStringLiteral("label_6"));
+        label_6->setGeometry(QRect(-10, -20, 901, 521));
+        label_6->setPixmap(QPixmap(QString::fromUtf8("../../../../bandes-seules-occultant-beige-clair-3515.jpg")));
+        label_15 = new QLabel(tab_4);
+        label_15->setObjectName(QStringLiteral("label_15"));
+        label_15->setGeometry(QRect(6, 0, 621, 371));
+        label_15->setPixmap(QPixmap(QString::fromUtf8("../../../Desktop/bg de construction \342\200\223 Recherche\302\240Google_files/54919363-gestionnaire-homme-builder-et-travailleur-sur-la-constructions-de-b\303\242timents-de-fond-construction-et-.jpg")));
+        label_15->setScaledContents(true);
+        tabWidget->addTab(tab_4, QString());
+        label_15->raise();
+        label_6->raise();
+        id_modif->raise();
+        modif_button->raise();
+        nom_modif->raise();
+        prix_modif->raise();
+        label_8->raise();
+        label_9->raise();
+        label_10->raise();
+        label_5 = new QLabel(centralWidget);
+        label_5->setObjectName(QStringLiteral("label_5"));
+        label_5->setGeometry(QRect(-10, -20, 901, 521));
+        label_5->setPixmap(QPixmap(QString::fromUtf8("../../../../bandes-seules-occultant-beige-clair-3515.jpg")));
+        MainWindow->setCentralWidget(centralWidget);
+        label_5->raise();
+        tabWidget->raise();
+        menuBar = new QMenuBar(MainWindow);
+        menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 679, 22));
+        MainWindow->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(MainWindow);
+        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
+        MainWindow->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(MainWindow);
+        statusBar->setObjectName(QStringLiteral("statusBar"));
+        MainWindow->setStatusBar(statusBar);
+
+        retranslateUi(MainWindow);
+
+        tabWidget->setCurrentIndex(3);
+
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "Gestion des Clients", Q_NULLPTR));
+        groupBox->setTitle(QString());
+        label->setText(QApplication::translate("MainWindow", "<html><head/><body><p><span style=\" font-size:14pt; color:#ff0000;\">Nom_produit</span></p></body></html>", Q_NULLPTR));
+        label_2->setText(QApplication::translate("MainWindow", "<html><head/><body><p><span style=\" font-size:14pt; color:#ff0000;\">id</span></p></body></html>", Q_NULLPTR));
+        label_3->setText(QApplication::translate("MainWindow", "<html><head/><body><p><span style=\" font-size:14pt; color:#ff0000;\">Prix</span></p></body></html>", Q_NULLPTR));
+        pushButton_2->setText(QApplication::translate("MainWindow", "ajouter", Q_NULLPTR));
+        label_13->setText(QString());
+        label_12->setText(QString());
+        tabWidget->setTabText(tabWidget->indexOf(tab), QApplication::translate("MainWindow", "Nouvel stock", Q_NULLPTR));
+        label_11->setText(QString());
+        label_16->setText(QString());
+        tabWidget->setTabText(tabWidget->indexOf(tab_2), QApplication::translate("MainWindow", "afficher des stock", Q_NULLPTR));
+        label_4->setText(QApplication::translate("MainWindow", "<html><head/><body><p><span style=\" font-size:14pt; color:#ff0000;\">id</span></p></body></html>", Q_NULLPTR));
+        pushButton_3->setText(QApplication::translate("MainWindow", "supprimer", Q_NULLPTR));
+        label_7->setText(QString());
+        label_14->setText(QString());
+        tabWidget->setTabText(tabWidget->indexOf(tab_3), QApplication::translate("MainWindow", "Eliminer un stock", Q_NULLPTR));
+        id_modif->setText(QString());
+        modif_button->setText(QApplication::translate("MainWindow", "modifier", Q_NULLPTR));
+        nom_modif->setText(QString());
+        prix_modif->setText(QString());
+        label_8->setText(QApplication::translate("MainWindow", "<html><head/><body><p><span style=\" font-size:14pt; color:#ff0000;\">Prix</span></p></body></html>", Q_NULLPTR));
+        label_9->setText(QApplication::translate("MainWindow", "<html><head/><body><p><span style=\" font-size:14pt; color:#ff0000;\">Nom_produit</span></p></body></html>", Q_NULLPTR));
+        label_10->setText(QApplication::translate("MainWindow", "<html><head/><body><p><span style=\" font-size:14pt; color:#ff0000;\">id</span></p></body></html>", Q_NULLPTR));
+        label_6->setText(QString());
+        label_15->setText(QString());
+        tabWidget->setTabText(tabWidget->indexOf(tab_4), QApplication::translate("MainWindow", "modifier", Q_NULLPTR));
+        label_5->setText(QString());
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
